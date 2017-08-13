c9898e1b23abe0ccd94ed98ba48164471b4a8e35
stephen powis <spowis@salesforce.com>
2017-08-13 18:30:06 +0900

    code style

--- src/main/java/com/darksci/pardot/api/parser/ResponseParser.java
+++ src/main/java/com/darksci/pardot/api/parser/ResponseParser.java
@@ -3,14 +3,15 @@ package com.darksci.pardot.api.parser;
 import java.io.IOException;
 
 /**
- *
+ * Interface Parsers for API responses implement.
+ * @param <T> The object type returned from the parser implementation.
  */
 public interface ResponseParser<T> {
 
     /**
-     * Abstract parseResponse method.  We give you the String representation of the response, your job is
-     * to parse into something more useful and return it!
-     * @param responseStr The response string from the request.
+     * The response from the request is given in String representation.  Implementation should
+     * parse into something more useful and return it!
+     * @param responseStr The response string from the request.     
      * @return Parsed response.
      * @throws IOException on parse errors.
      */
