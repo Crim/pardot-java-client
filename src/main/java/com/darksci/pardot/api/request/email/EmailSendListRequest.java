13e0017d64121372f18c8ea6dad9509c92a1be0d
Stephen Powis <spowis@salesforce.com>
2019-12-12 17:02:07 +0900

    cleanup

--- src/main/java/com/darksci/pardot/api/request/email/EmailSendListRequest.java
+++ src/main/java/com/darksci/pardot/api/request/email/EmailSendListRequest.java
@@ -20,7 +20,6 @@ package com.darksci.pardot.api.request.email;
 import com.darksci.pardot.api.request.BaseRequest;
 import org.joda.time.DateTime;
 
-import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.HashSet;
 
